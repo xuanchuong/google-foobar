f887a04bdbd712e960e725126f63c67809be0532
Chuong.Nguyen <chuong.nguyen@nakivo.com>
2021-09-25 11:46:13 +0700

    numbers-station-coded-messages: python version - pass all the tests

--- numbers-station-coded-messages/solution.py
+++ numbers-station-coded-messages/solution.py
@@ -1,19 +1,20 @@
 def solution(l, t):
     startIdx = -1
     endIdx = -1
+    messageLen = len(l)
+
     for idx, val in enumerate(l):
-        subTol = val
-        nextIdx = idx
-        while subTol < t and nextIdx < len(l)-1:
-            nextIdx += 1
-            subTol += l[nextIdx]
-        if subTol == t:
+        subTotal = val
+        subEndIdx = idx
+        while subTotal < t and subEndIdx < messageLen-1:
+            subEndIdx += 1
+            subTotal += l[subEndIdx]
+        if subTotal == t:
             startIdx = idx
-            endIdx = nextIdx
+            endIdx = subEndIdx
             break
 
     return [startIdx, endIdx]
-
 
 print(solution([1, 2, 3, 4], 15))
 print(solution([4, 3, 10, 2, 8], 12))
